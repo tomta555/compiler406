e394aff0298c7a123fa79c06b3f3634b9b827cbc
Thanawong <thanawong.saneewong@gmail.com>
2020-10-15 23:13:07 +0700

    Add many to many variable assignment

---
+++ compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/Left_id.java
@@ -0,0 +1,5 @@
+package th.ac.cmu.cpe.cpe406.ast;
+
+public interface Left_id extends Expr {
+
+}
--- compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/Right_expr_c.java
+++ compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/Right_expr_c.java
@@ -2,14 +2,15 @@ package th.ac.cmu.cpe.cpe406.ast;
 
 import th.ac.cmu.cpe.cpe406.util.Position;
 
-public class Swap_c extends Stmt_c implements Swap {
+public class Right_expr_c extends Expr_c implements Right_expr{
 	
 	Expr l;
 	Expr r;
-
-	public Swap_c(Position pos, Expr l, Expr r) {
+	
+	public Right_expr_c(Position pos, Expr l, Expr r) {
 		super(pos);
 		this.l = l;
 		this.r = r;
 	}
+
 }
