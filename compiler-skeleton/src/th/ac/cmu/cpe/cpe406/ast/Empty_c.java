6f03b5ceb9d97f4263b04df6a5f0f3791f01ac93
Thanawong <thanawong.saneewong@gmail.com>
2020-11-06 18:46:51 +0700

    implement if, if-else, empty-stmt

---
+++ compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/Empty_c.java
@@ -0,0 +1,12 @@
+package th.ac.cmu.cpe.cpe406.ast;
+
+import th.ac.cmu.cpe.cpe406.util.Position;
+
+public class Empty_c extends Stmt_c implements Empty {
+
+	public Empty_c(Position pos) {
+		super(pos);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/If_c.java
@@ -0,0 +1,25 @@
+package th.ac.cmu.cpe.cpe406.ast;
+
+import th.ac.cmu.cpe.cpe406.util.Position;
+
+public class If_c extends Stmt_c implements If {
+	protected Expr cond;
+    protected Stmt consequent;
+    protected Stmt alternative;
+    
+	public If_c(Position pos, Expr cond, Stmt consequent) {
+		super(pos);
+        assert cond != null && consequent != null;
+        this.cond = cond;
+        this.consequent = consequent;
+	}
+
+	public If_c(Position pos, Expr cond, Stmt consequent, Stmt alternative) {
+		super(pos);
+        assert cond != null && consequent != null; // alternative may be null;
+        this.cond = cond;
+        this.consequent = consequent;
+        this.alternative = alternative;
+	}
+
+}
