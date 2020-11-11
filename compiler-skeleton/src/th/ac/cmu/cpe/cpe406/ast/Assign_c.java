01e229dfaa30fe5571fcd31eb17c37b072030eb6
Thanawong <thanawong.saneewong@gmail.com>
2020-11-11 18:36:54 +0700

    implement block, fix multiple assignment

--- compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/Assign_c.java
+++ compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/Assign_c.java
@@ -1,13 +1,14 @@
 package th.ac.cmu.cpe.cpe406.ast;
 
+import java.util.List;
 import th.ac.cmu.cpe.cpe406.util.Position;
 
 public class Assign_c extends Stmt_c implements Assign {
 
-    Expr l;
-    Expr r;
+	List<Variable> l;
+	List<Expr> r;
 
-    public Assign_c(Position pos, Expr l, Expr r) {
+    public Assign_c(Position pos, List<Variable> l, List<Expr> r) {
         super(pos);
         this.l = l;
         this.r = r;
---
+++ compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/Block_c.java
@@ -0,0 +1,15 @@
+package th.ac.cmu.cpe.cpe406.ast;
+
+import th.ac.cmu.cpe.cpe406.util.Position;
+import java.util.List;
+
+public class Block_c extends Stmt_c implements Block {
+
+	protected List<Stmt> statements;
+	public Block_c(Position pos, List<Stmt> statements) {
+        super(pos);
+        assert (statements != null);
+        this.statements = statements;
+    }
+
+}
