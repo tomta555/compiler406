03ccb800ccffb67548694550cbee38574a2bc1e0
unknown <krit.upanun@gmail.com>
2020-11-17 21:33:52 +0700

    parser git 1 dim

--- compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/Array_c.java
+++ compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/Array_c.java
@@ -1,16 +1,27 @@
 package th.ac.cmu.cpe.cpe406.ast;
 
+import th.ac.cmu.cpe.cpe406.types.SymTable;
+import th.ac.cmu.cpe.cpe406.types.Type;
 import th.ac.cmu.cpe.cpe406.util.Position;
 
 public class Array_c extends TypeNode_c implements Array {
+
 	protected TypeNode type;
     protected Integer dim;
-	public Array_c(Position pos, TypeNode type ,Integer dim) {
+	public Array_c(Position pos, TypeNode type) {
         super(pos);
         assert (type != null);
         this.type = type;
-        this.dim = dim;
     }
-
+	@Override
+	public Type typeCheck(SymTable sym) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public SymTable BuildSymbolTable(SymTable sym) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
